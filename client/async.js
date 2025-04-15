99c3704cfbf711d5f051f58c61585ddba1e8d7b5
Dhanushraj07 <dhanushdharmalingam123@gmail.com>
2025-04-15 09:41:11 +0530

    modified demo.js

--- client/async.js
+++ client/async.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:client/async.js
 async function sendPostData() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
@@ -27,53 +26,3 @@ async function sendPostData() {
 }
 
 sendPostData();
-=======
-//GET METHOD
-
-async function getUsers() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    console.log("GET Method");
-    //console.log(data);
-    data.forEach(element => {
-      console.log(element.name);
-    });
-  } catch (error) {
-    console.error("Error fetching users:", error);
-  }
-}
-
-getUsers();
- 
-
-// async function sendPostData() {
-//   try {
-//     const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-//       method: 'POST', // Request method
-//       headers: {
-//         'Content-Type': 'application/json', // Sending JSON data
-//         'Authorization': 'Bearer fakeToken123' // Optional: if API needs a token
-//       },
-//       body: JSON.stringify({
-//         title: 'Hello World',
-//         body: 'This is a test post from fetch with async/await',
-//         userId: 1
-//       })
-//     });
-
-//     // Optional: Check if request was successful
-//     if (!response.ok) {
-//       throw new Error(`HTTP Error! status: ${response.status}`);
-//     }
-
-//     const data = await response.json(); // Parse the response JSON
-//     console.log("POST Method");
-//     console.log('POST response:', data);
-//   } catch (error) {
-//     console.error('❌ Error sending POST request:', error.message);
-//   }
-// }
-
-// sendPostData();
->>>>>>> 8a506f30b73550af53922d869435b35dcdf58883:async.js
--- client/demo.js
+++ client/demo.js
@@ -1,14 +1,11 @@
-<<<<<<< HEAD:client/demo.js
-const a =2;
-a=3;
-console.log(a);
-=======
-// const nums = ['1', '2', '3'];
-// const doubled = nums.map(n => n ); // [2, 4, 6]
-// const filtered = nums.filter(n => n%2); // [2]);
-// const sum = nums.reduce((acc, cur) => acc + cur, ''); // 6
 
-// console.log(sum); // Output: [2, 4, 6]
 
-// Output: ['Note 1', 'Note 2', 'Note 3']
->>>>>>> 8a506f30b73550af53922d869435b35dcdf58883:demo.js
+const nums = ['1', '2', '3'];
+const doubled = nums.map(n => n ); // [2, 4, 6]
+const filtered = nums.filter(n => n%2); // [2]);
+const sum = nums.reduce((acc, cur) => acc + cur, ''); // 6
+
+console.log(sum); // Output: [2, 4, 6]
+
+Output: ['Note 1', 'Note 2', 'Note 3']
+
